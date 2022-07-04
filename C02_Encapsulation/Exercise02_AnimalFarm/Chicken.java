68b8d0355c15fb86030e97e15e10d87cd3bfcc8c
Diana Angelova <diana.anguelova@gmail.com>
2022-07-04 21:54:41 +0300

    Add files via upload

---
+++ C02_Encapsulation/Exercise02_AnimalFarm/Chicken.java
@@ -0,0 +1,54 @@
+package C02_Encapsulation.Exercise02_AnimalFarm;
+
+public class Chicken {
+
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    private void setName(String name) {
+        if (name.trim().length() == 0 || name.equals(null)) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if (age < 0 || age > 15) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+    }
+
+    private double calculateProductPerDay() {
+        double numberEggs;
+        if (age <= 5) {
+            numberEggs = 2.00;
+        } else if (age >= 6 && age <= 11) {
+            numberEggs = 1.00;
+        } else numberEggs = 0.75;
+
+        return numberEggs;
+    }
+
+    public double productPerDay() {
+        return calculateProductPerDay();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken %s (age %d) can produce %.2f eggs per day.", name, age, productPerDay());
+    }
+}
---
+++ C02_Encapsulation/Exercise02_AnimalFarm/Main.java
@@ -0,0 +1,20 @@
+package C02_Encapsulation.Exercise02_AnimalFarm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+
+        try {
+            Chicken chicken = new Chicken(name, age);
+            System.out.println(chicken.toString());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
