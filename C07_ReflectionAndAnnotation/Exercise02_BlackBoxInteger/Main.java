96d20de0de77c568d9b5003e94a8502bfebfd0d6
Diana Angelova <diana.anguelova@gmail.com>
2022-07-26 10:11:27 +0300

    Add files via upload

---
+++ C07_ReflectionAndAnnotation/Exercise02_BlackBoxInteger/Main.java
@@ -0,0 +1,71 @@
+package C07_ReflectionAndAnnotation.Exercise02_BlackBoxInteger;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchFieldException {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Class<BlackBoxInt> blackBoxClass = BlackBoxInt.class;
+
+        List<Method> methods = Arrays.asList(blackBoxClass.getDeclaredMethods());
+
+        Constructor<BlackBoxInt> constructor = blackBoxClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        BlackBoxInt blackBoxInt = constructor.newInstance();
+
+        Field innerValue = blackBoxClass.getDeclaredField("innerValue");
+        innerValue.setAccessible(true);
+
+        while (!"END".equals(input)) {
+
+            String command = input.split("_")[0];
+            int number = Integer.parseInt(input.split("_")[1]);
+
+            switch (command) {
+                case "add":
+                    executeCommand(blackBoxInt, number, innerValue, "add", methods);
+                    break;
+                case "subtract":
+                    executeCommand(blackBoxInt, number, innerValue, "subtract", methods);
+                    break;
+                case "multiply":
+                    executeCommand(blackBoxInt, number, innerValue, "multiply", methods);
+                    break;
+                case "divide":
+                    executeCommand(blackBoxInt, number, innerValue, "divide", methods);
+                    break;
+                case "leftShift":
+                    executeCommand(blackBoxInt, number, innerValue, "leftShift", methods);
+                    break;
+                case "rightShift":
+                    executeCommand(blackBoxInt, number, innerValue, "rightShift", methods);
+                    break;
+            }
+            input = scanner.nextLine();
+        }
+    }
+
+    private static void executeCommand(BlackBoxInt blackBoxInt, int number, Field innerValue, String command, List<Method> methods) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method currentMethod = null;
+        for (Method method : methods) {
+            if (method.getName().equals(command)) {
+                currentMethod = method;
+                break;
+            }
+        }
+
+        currentMethod.setAccessible(true);
+
+        currentMethod.invoke(blackBoxInt, number);
+        System.out.println(innerValue.get(blackBoxInt));
+    }
+}
