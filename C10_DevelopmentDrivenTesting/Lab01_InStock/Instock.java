803ac4c28774280f999b833740481531955f2f55
Diana Angelova <diana.anguelova@gmail.com>
2022-08-03 15:56:24 +0300

    Add files via upload

---
+++ C10_DevelopmentDrivenTesting/Lab01_InStock/Instock.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Instock implements ProductStock {
+    private List<Product> products;
+
+    public Instock() {
+        this.products = new ArrayList<>();
+    }
+
+    @Override
+    public int getCount() {
+        return products.size();
+    }
+
+    @Override
+    public boolean contains(Product product) {
+        return products.contains(product);
+    }
+
+    @Override
+    public void add(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public void changeQuantity(String label, int newQuantity) {
+        Product product = findByLabel(label);
+
+        product.setQuantity(newQuantity);
+    }
+
+    @Override
+    public Product find(int index) {
+        if (index > products.size() - 1 || index < 0)
+            throw new IndexOutOfBoundsException();
+        return products.get(index);
+    }
+
+    @Override
+    public Product findByLabel(String label) {
+        return products.stream()
+                .filter(p -> p.getLabel().equals(label))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+    @Override
+    public Iterable<Product> findFirstByAlphabeticalOrder(int count) {
+        if (count > getCount() || count < 0) {
+            throw new IllegalArgumentException();
+        }
+            return products.stream()
+                    .sorted(Comparator.comparing(p -> p.getLabel()))
+                    .limit(count)
+                    .collect(Collectors.toList());}
+
+    @Override
+    public Iterable<Product> findAllInRange(double lo, double hi) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterable<Product> findAllByPrice(double price) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterable<Product> findFirstMostExpensiveProducts(int count) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterable<Product> findAllByQuantity(int quantity) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<Product> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
