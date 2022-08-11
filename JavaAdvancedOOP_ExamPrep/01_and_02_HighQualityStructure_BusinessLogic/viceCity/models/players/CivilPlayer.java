3c450e81eb632c4a542b3a80a3427c63e17d3edf
Diana Angelova <diana.anguelova@gmail.com>
2022-08-11 17:40:51 +0300

    Delete JavaAdvancedOOP_ExamPrep/01_and_02_HighQualityStructure_BusinessLogic/viceCity directory

--- JavaAdvancedOOP_ExamPrep/01_and_02_HighQualityStructure_BusinessLogic/viceCity/models/players/CivilPlayer.java
+++
@@ -1,9 +0,0 @@
-package viceCity.models.players;
-
-public class CivilPlayer extends BasePlayer {
-    public static final int CIVIL_PLAYER_LIFE_POINTS = 50;
-
-    public CivilPlayer(String name) {
-        super(name, CIVIL_PLAYER_LIFE_POINTS);
-    }
-}
