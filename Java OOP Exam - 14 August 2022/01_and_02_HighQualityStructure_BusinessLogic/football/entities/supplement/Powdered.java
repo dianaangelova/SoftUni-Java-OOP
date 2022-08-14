80d697b6a7d83029a6e901b52206cf57c2a0ce1f
Diana Angelova <diana.anguelova@gmail.com>
2022-08-14 23:32:04 +0300

    Add files via upload

---
+++ Java OOP Exam - 14 August 2022/01_and_02_HighQualityStructure_BusinessLogic/football/entities/supplement/Powdered.java
@@ -0,0 +1,10 @@
+package football.entities.supplement;
+
+public class Powdered extends BaseSupplement {
+    public static final int INITIAL_ENERGY = 120;
+    public static final double INITIAL_PRICE = 15;
+
+    public Powdered() {
+        super(INITIAL_ENERGY, INITIAL_PRICE);
+    }
+}
